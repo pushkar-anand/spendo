73c833823fe15b73b4df3ce43ac35a2ef031393c
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-26 22:25:38 +0530

    test dependencies.

--- app/src/androidTest/java/me/pushkaranand/spendo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/me/pushkaranand/spendo/ExampleInstrumentedTest.kt
@@ -1,5 +1,7 @@
 package me.pushkaranand.spendo
 
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
