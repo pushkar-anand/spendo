f28d38f24fc99315ea67f3ba095d2a928058f093
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-01 14:09:20 +0530

    Changed home activity to a bottom navigation.

--- app/src/main/java/me/pushkaranand/spendo/ui/HomeActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/HomeActivity.kt
@@ -3,14 +3,37 @@ package me.pushkaranand.spendo.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home.*
 import me.pushkaranand.spendo.R
 import me.pushkaranand.spendo.helpers.PrefHelper
 
 class HomeActivity : AppCompatActivity() {
 
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    message.setText(R.string.title_home)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_dashboard -> {
+                    message.setText(R.string.title_dashboard)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notifications -> {
+                    message.setText(R.string.title_notifications)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val prefHelper = PrefHelper(this)
 
