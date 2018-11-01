c05f0b54fc22411ac0c85bce48c42b57a9452674
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-01 11:11:00 +0530

    Finished intro slider dummy design and logic

---
+++ app/src/main/java/me/pushkaranand/spendo/adapters/IntroSliderPagerAdapter.kt
@@ -0,0 +1,36 @@
+package me.pushkaranand.spendo.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import me.pushkaranand.spendo.R
+
+class IntroSliderPagerAdapter internal constructor(private val context: Context) : PagerAdapter() {
+    public val layouts: Array<Int> =
+        arrayOf(R.layout.intro_slide_one, R.layout.intro_slide_two, R.layout.intro_slide_three)
+
+    override fun instantiateItem(container: ViewGroup, position: Int): View {
+
+        val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val view = layoutInflater.inflate(layouts[position], container, false)
+        container.addView(view)
+
+        return view
+    }
+
+    override fun getCount(): Int {
+        return layouts.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val view = `object` as View
+        container.removeView(view)
+    }
+}
--- app/src/main/java/me/pushkaranand/spendo/ui/IntroSliderActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/IntroSliderActivity.kt
@@ -1,13 +1,119 @@
 package me.pushkaranand.spendo.ui
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_intro_slider.*
 import me.pushkaranand.spendo.R
+import me.pushkaranand.spendo.adapters.IntroSliderPagerAdapter
+
 
 class IntroSliderActivity : AppCompatActivity() {
+
+    private var dots: Array<TextView?>? = null
+    var pagerAdapter: IntroSliderPagerAdapter? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_slider)
+
+        pagerAdapter = IntroSliderPagerAdapter(this)
+
+        changeStatusBarColor()
+        addBottomDots(0)
+
+        val viewPager: ViewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = pagerAdapter
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener)
+
+
+        skipBtn.setOnClickListener { _ ->
+            launchApp()
+        }
+
+        nextBtn.setOnClickListener { _ ->
+            val current: Int = getItem(+1)
+            if (current < pagerAdapter!!.layouts.size) {
+                viewPager.currentItem = current
+            } else {
+                launchApp()
+            }
+        }
+
+    }
+
+    private fun launchApp() {
+
+    }
+
+    private fun getItem(i: Int): Int {
+        return viewPager.currentItem + i
+    }
+
+    private fun changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    private var viewPagerPageChangeListener: ViewPager.OnPageChangeListener = object : ViewPager.OnPageChangeListener {
+
+        override fun onPageSelected(position: Int) {
+            addBottomDots(position)
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == (pagerAdapter!!.layouts.size - 1)) {
+                // last page. make button text to GOT IT
+                nextBtn.text = getString(R.string.start)
+                skipBtn.visibility = View.GONE
+
+            } else {
+                // still pages are left
+                nextBtn.text = getString(R.string.next)
+                skipBtn.visibility = View.VISIBLE
+            }
+        }
+
+        override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {
+
+        }
+
+        override fun onPageScrollStateChanged(arg0: Int) {
+
+        }
+    }
+
+    private fun addBottomDots(currentPage: Int) {
+        dots = arrayOfNulls(pagerAdapter!!.layouts.size)
+
+        val colorsActive = resources.getIntArray(R.array.array_dot_active)
+        val colorsInactive = resources.getIntArray(R.array.array_dot_inactive)
+
+        layoutDots.removeAllViews()
+
+        for (i in 0 until dots!!.size) {
+            dots!![i] = TextView(this)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                dots!![i]!!.text = Html.fromHtml("&#8226;", Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                @Suppress("DEPRECATION")
+                dots!![i]!!.text = Html.fromHtml("&#8226;")
+            }
+            dots!![i]!!.textSize = 35F
+            dots!![i]!!.setTextColor(colorsInactive[currentPage])
+            layoutDots.addView(dots!![i])
+        }
+
+        if (dots!!.isNotEmpty()) {
+            dots!![currentPage]!!.setTextColor(colorsActive[currentPage])
+        }
     }
 }
