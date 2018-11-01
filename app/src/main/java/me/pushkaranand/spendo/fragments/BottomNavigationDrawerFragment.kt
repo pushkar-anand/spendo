fb1e2b945f713e6af4d6eb8d32931470d8154014
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-01 19:31:45 +0530

    working on navigation items

--- app/src/main/java/me/pushkaranand/spendo/fragments/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/me/pushkaranand/spendo/fragments/BottomNavigationDrawerFragment.kt
@@ -1,9 +1,11 @@
 package me.pushkaranand.spendo.fragments
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import me.pushkaranand.spendo.R
 
@@ -13,5 +15,9 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bottomsheet, container, false)
+        //return MenuBinding.inflate(inflater, container, false).root
     }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        BottomSheetDialog(requireContext(), theme)
 }
