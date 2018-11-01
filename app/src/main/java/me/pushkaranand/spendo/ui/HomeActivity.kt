f47574dea70a15399b984cc7843aaaf5637f7f11
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-01 11:12:45 +0530

    added new home activity.

---
+++ app/src/main/java/me/pushkaranand/spendo/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package me.pushkaranand.spendo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.pushkaranand.spendo.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
