40cff1f1d28ab45df363c8b6c7b1c4c148a49c5e
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-01 17:49:28 +0530

    Finshed dummy design of bottom app bar.

---
+++ app/src/main/java/me/pushkaranand/spendo/fragments/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,17 @@
+package me.pushkaranand.spendo.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import me.pushkaranand.spendo.R
+
+class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bottomsheet, container, false)
+    }
+}
--- app/src/main/java/me/pushkaranand/spendo/ui/HomeActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/HomeActivity.kt
@@ -2,8 +2,12 @@ package me.pushkaranand.spendo.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
 import me.pushkaranand.spendo.R
+import me.pushkaranand.spendo.fragments.BottomNavigationDrawerFragment
 import me.pushkaranand.spendo.helpers.PrefHelper
 
 class HomeActivity : AppCompatActivity() {
@@ -11,6 +15,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(bottomAppBar)
+
 
         val prefHelper = PrefHelper(this)
 
@@ -19,6 +25,22 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_primary_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home -> {
+                val bottomNavDrawerFragment = BottomNavigationDrawerFragment()
+                bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+            }
+        }
+        return true
     }
 
 }
