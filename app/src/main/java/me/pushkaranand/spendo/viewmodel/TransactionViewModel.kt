f7628398ede677fc92185d23d40e80fc2fe0b374
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-03 21:42:00 +0530

    .java to .kt

---
+++ app/src/main/java/me/pushkaranand/spendo/viewmodel/TransactionViewModel.kt
@@ -0,0 +1,23 @@
+package me.pushkaranand.spendo.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import me.pushkaranand.spendo.db.entity.Transaction
+import me.pushkaranand.spendo.repository.TransactionRepository
+
+
+class TransactionViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val transactionRepository: TransactionRepository = TransactionRepository(application)
+    private val allTransactions: LiveData<ArrayList<Transaction>>?
+
+    init {
+        allTransactions = transactionRepository.getAllTransactions()
+    }
+
+    fun insert(transaction: Transaction) {
+        transactionRepository.insert(transaction)
+    }
+
+}
