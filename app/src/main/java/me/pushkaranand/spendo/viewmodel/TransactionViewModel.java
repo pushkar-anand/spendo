4f4e19b6b963aec446dd0545be5f4a892a4e3343
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-03 21:32:59 +0530

    added viewmodel for transaction.

---
+++ app/src/main/java/me/pushkaranand/spendo/viewmodel/TransactionViewModel.java
@@ -0,0 +1,11 @@
+package me.pushkaranand.spendo.viewmodel;
+
+import androidx.lifecycle.AndroidViewModel;
+import me.pushkaranand.spendo.repository.TransactionRepository;
+
+public class TransactionViewModel extends AndroidViewModel {
+
+    private TransactionRepository transactionRepository;
+
+    
+}
