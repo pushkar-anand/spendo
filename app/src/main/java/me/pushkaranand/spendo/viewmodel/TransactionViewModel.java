f623dcd4cc8166ba694cca4d2fb5c2721e7fdac6
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-03 21:39:33 +0530

    finished viewmodel for transaction.

--- app/src/main/java/me/pushkaranand/spendo/viewmodel/TransactionViewModel.java
+++ app/src/main/java/me/pushkaranand/spendo/viewmodel/TransactionViewModel.java
@@ -1,11 +1,30 @@
 package me.pushkaranand.spendo.viewmodel;
 
+import android.app.Application;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import me.pushkaranand.spendo.db.entity.Transaction;
 import me.pushkaranand.spendo.repository.TransactionRepository;
+
+import java.util.ArrayList;
 
 public class TransactionViewModel extends AndroidViewModel {
 
     private TransactionRepository transactionRepository;
+    private LiveData<ArrayList<Transaction>> allTransactions;
 
-    
+    public TransactionViewModel(Application application) {
+        super(application);
+        transactionRepository = new TransactionRepository(application);
+        allTransactions = transactionRepository.getAllTransactions();
+    }
+
+    public LiveData<ArrayList<Transaction>> getAllTransactions() {
+        return allTransactions;
+    }
+
+    public void insert(Transaction transaction) {
+        transactionRepository.insert(transaction);
+    }
+
 }
