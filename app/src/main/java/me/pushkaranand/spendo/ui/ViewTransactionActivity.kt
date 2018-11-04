cfca112fda2d55fe70fd323e020ea838a7f220a4
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-04 20:20:12 +0530

    linked to view model.

--- app/src/main/java/me/pushkaranand/spendo/ui/ViewTransactionActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/ViewTransactionActivity.kt
@@ -2,12 +2,23 @@ package me.pushkaranand.spendo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import me.pushkaranand.spendo.R
+import me.pushkaranand.spendo.viewmodel.TransactionViewModel
 
 class ViewTransactionActivity : AppCompatActivity() {
+
+    private var transactionViewModel: TransactionViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_transaction)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        transactionViewModel =
+                ViewModelProviders.of(this).get(TransactionViewModel::class.java)
     }
 }
