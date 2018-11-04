3091ce9478a416a4938a2f84aad37b64b57bbf1e
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-04 20:14:17 +0530

    added activity for category display.

---
+++ app/src/main/java/me/pushkaranand/spendo/ui/CategoryDisplayActivity.kt
@@ -0,0 +1,13 @@
+package me.pushkaranand.spendo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.pushkaranand.spendo.R
+
+class CategoryDisplayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_display)
+    }
+}
