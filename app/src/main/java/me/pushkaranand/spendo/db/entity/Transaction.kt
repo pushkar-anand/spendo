ff83f7e3cbcb51cb44fe4c4733f9a19ba05146f8
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-04 18:07:53 +0530

    schema changes.

--- app/src/main/java/me/pushkaranand/spendo/db/entity/Transaction.kt
+++ app/src/main/java/me/pushkaranand/spendo/db/entity/Transaction.kt
@@ -1,28 +1,27 @@
 package me.pushkaranand.spendo.db.entity
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 
 @Entity(
-    tableName = "transactions",
-    foreignKeys = [ForeignKey(
-        entity = Category::class,
-        parentColumns = ["categoryID"], childColumns = ["transactionID"]
-    )]
+    tableName = "transactions"
 )
 data class Transaction(
     var amount: Double,
     var type: String,
     var category: String,
-    var category_id: Int,
     var date: String,
-    var notes: String?,
-    var day: String,
-    var year: Int,
-    var month: String
+    var notes: String?
 ) {
     @PrimaryKey(autoGenerate = true)
     var transactionID: Long = 0
+
+    @Ignore
+    private val spilt = date.split("-")
+
+    var day: Int = spilt[0].toInt()
+    var month: String = spilt[1]
+    var year: Int = spilt[2].toInt()
 }
