6fbdcc9cf8887f065a7dcaba349f9f7b73e2d8d7
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-04 20:15:06 +0530

    added activity for settings

---
+++ app/src/main/java/me/pushkaranand/spendo/ui/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package me.pushkaranand.spendo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.pushkaranand.spendo.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
