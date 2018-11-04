31bb195148decc659adc04a12123dbb1d442079a
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-04 12:29:50 +0530

    using viewmodel of category in NewTransactionActivity.

--- app/src/main/java/me/pushkaranand/spendo/ui/NewTransactionActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/NewTransactionActivity.kt
@@ -2,12 +2,31 @@ package me.pushkaranand.spendo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import me.pushkaranand.spendo.R
+import me.pushkaranand.spendo.db.entity.Category
+import me.pushkaranand.spendo.viewmodel.CategoryViewModel
 
 class NewTransactionActivity : AppCompatActivity() {
+
+    private var categoryViewModel: CategoryViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_transaction)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        categoryViewModel =
+                ViewModelProviders.of(this).get(CategoryViewModel::class.java)
+    }
+
+    fun addCategoryChips() {
+        val categories: List<Category>? = categoryViewModel?.getAllCategories()
+        if (categories != null) {
+
+        }
     }
 }
