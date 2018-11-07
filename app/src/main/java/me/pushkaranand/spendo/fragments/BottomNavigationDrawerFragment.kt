375c64378b60488b90910a2528d72d6176f91a03
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-07 22:56:34 +0530

    linked home activity to settings activity.

--- app/src/main/java/me/pushkaranand/spendo/fragments/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/me/pushkaranand/spendo/fragments/BottomNavigationDrawerFragment.kt
@@ -1,13 +1,16 @@
 package me.pushkaranand.spendo.fragments
 
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_bottomsheet.*
 import me.pushkaranand.spendo.R
+import me.pushkaranand.spendo.ui.SettingsActivity
 
 class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
@@ -16,6 +19,22 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bottomsheet, container, false)
         //return MenuBinding.inflate(inflater, container, false).root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        bottomNavView.setNavigationItemSelectedListener { menuItem ->
+
+            when (menuItem.itemId) {
+                R.id.nav_settings -> {
+                    val intent = Intent(context, SettingsActivity::class.java)
+                    context!!.startActivity(intent)
+                }
+            }
+            true
+        }
+
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
