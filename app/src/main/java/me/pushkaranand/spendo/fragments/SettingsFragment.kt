53cd1ce31557f351d6cebf83d3fbc98a10e4862c
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-07 23:44:53 +0530

    added fragment backstack.

--- app/src/main/java/me/pushkaranand/spendo/fragments/SettingsFragment.kt
+++ app/src/main/java/me/pushkaranand/spendo/fragments/SettingsFragment.kt
@@ -1,11 +1,18 @@
 package me.pushkaranand.spendo.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 import me.pushkaranand.spendo.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.settings_pref);
+        setPreferencesFromResource(R.xml.settings_pref, rootKey)
     }
+
+    override fun getCallbackFragment(): Fragment {
+        return this
+    }
+
 }
--- app/src/main/java/me/pushkaranand/spendo/ui/SettingsActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/SettingsActivity.kt
@@ -2,9 +2,11 @@ package me.pushkaranand.spendo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceScreen
 import me.pushkaranand.spendo.fragments.SettingsFragment
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +15,18 @@ class SettingsActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(android.R.id.content, SettingsFragment())
             .commit()
+    }
+
+    override fun onPreferenceStartScreen(caller: PreferenceFragmentCompat?, pref: PreferenceScreen?): Boolean {
+
+        val ft = supportFragmentManager.beginTransaction()
+        val settingsFragment = SettingsFragment()
+        val args = Bundle()
+        args.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, pref?.key)
+        settingsFragment.arguments = args
+        ft.replace(android.R.id.content, settingsFragment, pref?.key)
+        ft.addToBackStack(pref?.key)
+        ft.commit()
+        return true
     }
 }
