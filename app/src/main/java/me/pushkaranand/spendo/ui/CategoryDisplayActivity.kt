7fc676bd508523608532e1e07406a53cdb014179
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-08 11:35:10 +0530

    linked adapter.

--- app/src/main/java/me/pushkaranand/spendo/ui/CategoryDisplayActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/CategoryDisplayActivity.kt
@@ -3,15 +3,20 @@ package me.pushkaranand.spendo.ui
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_category_display.*
+import kotlinx.android.synthetic.main.content_category_display.*
 import me.pushkaranand.spendo.R
+import me.pushkaranand.spendo.adapters.CategoriesRecyclerViewAdapter
 import me.pushkaranand.spendo.fragments.BottomNavigationDrawerFragment
 import me.pushkaranand.spendo.viewmodel.CategoryViewModel
 
 class CategoryDisplayActivity : AppCompatActivity() {
 
     private var categoryViewModel: CategoryViewModel? = null
+    private var adapter: CategoriesRecyclerViewAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,7 @@ class CategoryDisplayActivity : AppCompatActivity() {
         setSupportActionBar(bottomAppBarCategoryActivity)
         initViewModel()
         setupRecyclerView()
+        attachClickListeners()
         setupLiveObserver()
     }
 
@@ -33,7 +39,9 @@ class CategoryDisplayActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-
+        adapter = CategoriesRecyclerViewAdapter(this)
+        categoryRecyclerView.adapter = adapter
+        categoryRecyclerView.layoutManager = GridLayoutManager(this, 2)
     }
 
     private fun initViewModel() {
@@ -42,6 +50,14 @@ class CategoryDisplayActivity : AppCompatActivity() {
     }
 
     private fun setupLiveObserver() {
+        categoryViewModel?.getAllCategories()?.observe(this, Observer {
+            adapter?.setCategories(it)
+        })
+    }
 
+    private fun attachClickListeners() {
+        addCategoryBtn.setOnClickListener {
+
+        }
     }
 }
