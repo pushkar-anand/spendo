2674136c8bc001891de87aef4415751649acd6b4
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-08 20:01:43 +0530

    added sorting helper.

---
+++ app/src/main/java/me/pushkaranand/spendo/helpers/Sorting.kt
@@ -0,0 +1,30 @@
+package me.pushkaranand.spendo.helpers
+
+import me.pushkaranand.spendo.db.entity.Transaction
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Sorting {
+
+    companion object {
+        fun sortByDateDescending(transactions: ArrayList<Transaction>): ArrayList<Transaction> {
+
+            transactions.sortWith(comparator = Comparator { t1, t2 ->
+                val dateString1: String = t1.date
+                val dateString2: String = t2.date
+
+                if (dateString1 == dateString2) {
+                    return@Comparator t2.date.compareTo(t2.date)
+                } else {
+                    val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+
+                    val date1 = df.parse(dateString1)
+                    val date2 = df.parse(dateString2)
+                    return@Comparator date2.compareTo(date1)
+                }
+
+            })
+            return transactions
+        }
+    }
+}
