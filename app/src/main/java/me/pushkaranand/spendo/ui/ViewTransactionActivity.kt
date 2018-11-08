dff4d9e870739054d0b5a1364ebe530cbb09ca21
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-08 16:39:43 +0530

    Linked UI and data. Added menu.

--- app/src/main/java/me/pushkaranand/spendo/ui/ViewTransactionActivity.kt
+++ app/src/main/java/me/pushkaranand/spendo/ui/ViewTransactionActivity.kt
@@ -1,9 +1,13 @@
 package me.pushkaranand.spendo.ui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_view_transaction.*
+import kotlinx.android.synthetic.main.content_view_transaction.*
 import me.pushkaranand.spendo.R
 import me.pushkaranand.spendo.viewmodel.TransactionViewModel
 
@@ -15,10 +19,14 @@ class ViewTransactionActivity : AppCompatActivity() {
     companion object {
         const val TRANSACTION_ID = "me.pushkaranand.spendo.ui.TRANSACTION_ID"
     }
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_transaction)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_black);
+        supportActionBar?.title = "Transaction"
 
         if (!intent.hasExtra(TRANSACTION_ID)) {
             finish()
@@ -30,6 +38,12 @@ class ViewTransactionActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.view_transaction_menu, menu)
+        return true
+    }
+
     private fun initViewModel() {
         transactionViewModel =
                 ViewModelProviders.of(this).get(TransactionViewModel::class.java)
@@ -38,6 +52,21 @@ class ViewTransactionActivity : AppCompatActivity() {
     private fun setupViewModelObserver() {
         transactionViewModel?.getTransaction(transactionId!!)?.observe(this, Observer {
 
+            val t = it.amount.toString().replace("-", "")
+            var tmp = "${getString(R.string.rupee_symbol)}$t"
+            amountTextView.text = tmp
+
+            typeTextView.text = it.type
+
+            dateTextView.text = it.date
+
+            tmp = ""
+            val gson = Gson()
+            val list = gson.fromJson(it.category, ArrayList::class.java)
+            for (c in list) {
+                tmp += "$c\n"
+            }
+            categoriesTextView.text = tmp
         })
     }
 }
