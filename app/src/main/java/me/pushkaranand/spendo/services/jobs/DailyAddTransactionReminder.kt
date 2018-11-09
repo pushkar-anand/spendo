87cd527c61006f54ecb2379244b49bd2627ff228
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-09 20:08:19 +0530

    linked job to notification helper.

--- app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
+++ app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
@@ -2,11 +2,12 @@ package me.pushkaranand.spendo.services.jobs
 
 import com.firebase.jobdispatcher.JobParameters
 import com.firebase.jobdispatcher.JobService
+import me.pushkaranand.spendo.helpers.notifications.Notification
 
 class DailyAddTransactionReminder : JobService() {
 
     override fun onStartJob(job: JobParameters?): Boolean {
-
+        Notification.sendAddReminderNotification(applicationContext)
         return false
     }
 
