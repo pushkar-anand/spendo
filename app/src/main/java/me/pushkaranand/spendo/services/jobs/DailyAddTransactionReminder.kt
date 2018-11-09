ea1420fab163e283aaf63c10cf2c7ae067fe31ac
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-09 19:29:38 +0530

    Added firebase job dispatcher. Started building a job to notify daily add.

---
+++ app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
@@ -0,0 +1,18 @@
+package me.pushkaranand.spendo.services.jobs
+
+import com.firebase.jobdispatcher.JobParameters
+import com.firebase.jobdispatcher.JobService
+
+class DailyAddTransactionReminder : JobService() {
+
+    override fun onStartJob(job: JobParameters?): Boolean {
+
+        return false
+    }
+
+    override fun onStopJob(job: JobParameters?): Boolean {
+
+        return false
+    }
+
+}
