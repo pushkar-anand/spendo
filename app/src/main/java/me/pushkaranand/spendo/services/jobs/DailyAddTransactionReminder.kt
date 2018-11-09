7ae29ed775bbf01949ed9a8fc60bf66e0b0d4dec
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-09 22:12:49 +0530

    jobs now start schedules itself again.

--- app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
+++ app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
@@ -2,12 +2,20 @@ package me.pushkaranand.spendo.services.jobs
 
 import com.firebase.jobdispatcher.JobParameters
 import com.firebase.jobdispatcher.JobService
+import me.pushkaranand.spendo.helpers.JobHelpers
 import me.pushkaranand.spendo.helpers.notifications.Notification
+import java.util.*
 
 class DailyAddTransactionReminder : JobService() {
 
     override fun onStartJob(job: JobParameters?): Boolean {
         Notification.sendAddReminderNotification(applicationContext)
+
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minutes = calendar.get(Calendar.MINUTE)
+        val minutesAfterMidnight = (hour * 60) + minutes
+        JobHelpers.dispatchAddReminderJob(applicationContext, minutesAfterMidnight)
         return false
     }
 
