fd4ae6409e36aee09a369c48288804a6e794027d
Pushkar Anand <anandpushkar088@gmail.com>
2018-11-24 21:53:20 +0530

    added retry strategy to fix reminder job.

--- app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
+++ app/src/main/java/me/pushkaranand/spendo/services/jobs/DailyAddTransactionReminder.kt
@@ -9,19 +9,22 @@ import java.util.*
 class DailyAddTransactionReminder : JobService() {
 
     override fun onStartJob(job: JobParameters?): Boolean {
+
         Notification.sendAddReminderNotification(applicationContext)
 
         val calendar = Calendar.getInstance()
         val hour = calendar.get(Calendar.HOUR_OF_DAY)
         val minutes = calendar.get(Calendar.MINUTE)
         val minutesAfterMidnight = (hour * 60) + minutes
+
         JobHelpers.dispatchAddReminderJob(applicationContext, minutesAfterMidnight)
+
         return false
     }
 
     override fun onStopJob(job: JobParameters?): Boolean {
 
-        return false
+        return true
     }
 
 }
